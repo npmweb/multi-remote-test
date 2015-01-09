4960db8b6d9ee6016a1644f4cb627efe0ce0e260
Josh Justice <josh@need-bee.com>
2015-01-09 13:04:55 -0500

    initial public commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once('.env.php');
+
+?><!DOCTYPE html>
+<html>
+<head>
+<title>Multi-Remote Test</title>
+<link rel="stylesheet" type="text/css" href="includes/css/themes/<?php echo getenv('THEME') ?>.css" />
+</head>
+<body>
+Hello, world!
+</body>
+</html>
